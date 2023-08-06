3e65f15e5507915595c71b3de47d8ef409c5146d
Oscar Sandoval <oscar@daoai.ca>
2023-08-06 11:19:01 -0700

    Get arm config using timestep

---
+++ kinematics/forward_kin.py
@@ -0,0 +1,32 @@
+from typing import List
+
+import numpy as np
+import modern_robotics as mr
+
+class ForwardKin:
+    def __init__(self, init_q: List[float]):
+        self._init_q = np.array(init_q)
+        self._curr_arm_th = self._init_q.copy()
+        self._joint_max_speed = 9e9
+        self._joint_speeds = np.array([self._joint_max_speed] * len(self._init_q))
+
+    @property
+    def joint_max_speed(self):
+        return self._joint_max_speed
+
+    @joint_max_speed.setter
+    def joint_max_speed(self, value):
+        self._joint_max_speed = value
+
+    def get_new_arm_config(self, joint_speeds: List[float], timestep: float) -> List[float]:
+        # Enforce joint speed limits
+        j_speeds = np.array([min(speed, self._joint_max_speed) for speed in joint_speeds])
+
+        new_angles = self._curr_arm_th + j_speeds * timestep
+        return new_angles.tolist()
+
+    def update_arm_config(self, joint_speeds: List[float], timestep: float) -> List[float]:
+        self._curr_arm_th = self.get_new_arm_config(joint_speeds, timestep)
+        return self._curr_arm_th.copy()
+
+
---
+++ test/test_forward_kin.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from kinematics.forward_kin import ForwardKin
+
+
+class TestForwardKin(TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.arm = ForwardKin([0, 0, 0, 0, 0])
+
+    def test_get_new_arm_config(self):
+        new_config = self.arm.get_new_arm_config([0] * 5, 0)
+        self.assertListEqual(new_config, [0] * 5)
+
+        new_config = self.arm.get_new_arm_config([1, 0, 0, 0, 0], 1)
+        self.assertListEqual(new_config, [1, 0, 0, 0, 0])
+
+        new_config = self.arm.get_new_arm_config([1]*5, 1)
+        self.assertListEqual(new_config, [1, 1, 1, 1, 1])
+
+        new_config = self.arm.get_new_arm_config([1] * 5, 0.5)
+        self.assertListEqual(new_config, [0.5]*5)
+
+    def test_joint_speed_limit(self):
+        self.arm.joint_max_speed = 0.5
+        new_config = self.arm.get_new_arm_config([1] * 5, 1)
+        self.assertListEqual(new_config, [0.5]*5)
+
+        self.arm.joint_max_speed = 4
+        new_config = self.arm.get_new_arm_config([1,2,3,4,5], 1)
+        self.assertListEqual(new_config, [1,2,3,4,4])
+
+        self.arm.joint_max_speed = 2
+        new_config = self.arm.get_new_arm_config([1,2,3,4,5], 1)
+        self.assertListEqual(new_config, [1,2,2,2,2])
+
