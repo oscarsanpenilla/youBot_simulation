ef9a97c1661f48406fe79c0ddce113d450936367
Oscar Sandoval <oscar@daoai.ca>
2023-08-16 23:41:10 -0700

    Updated init robot config and grap angle

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from numpy import cos, sin, pi
 from plotter import plot_columns_from_csv
 
 def gen_pickup_dropoff_block_trajectory():
+    th = np.radians(-20)
+    rot_y = np.array([
+        [cos(th), 0, -sin(th), 0],
+        [0, 1, 0, 0],
+        [sin(th), 0, cos(th), 0],
+        [0, 0, 0, 1]
+    ])
+
     Tsc_init = np.array([
         [1, 0, 0, 1.0],
         [0, 1, 0, 0.0],
@@ -26,6 +34,7 @@ def gen_pickup_dropoff_block_trajectory():
         [-1,  0, 0, 0.1],
         [ 0,  0, 0, 1.0],
     ])
+    Tce_standoff = np.matmul(Tce_standoff, rot_y)
 
     Tce_grasp = np.array([
         [ 0,  0, 1, 0.0],
@@ -33,6 +42,7 @@ def gen_pickup_dropoff_block_trajectory():
         [-1,  0, 0, 0.025],
         [ 0,  0, 0, 1.0],
     ])
+    Tce_grasp = np.matmul(Tce_grasp, rot_y)
 
     T1 = np.matmul(Tsc_init, Tce_standoff)  # above block pre
     T2 = np.matmul(Tsc_init, Tce_grasp)  # move grasp
@@ -43,12 +53,12 @@ def gen_pickup_dropoff_block_trajectory():
     T7 = np.array(T6)  # release
     T8 = np.array(T5)  # move drop-off post
 
-    # goals = [T1]
-    # times = [10]
-    # gripper_states = [0]
-    goals = [T1, T2, T3, T4, T5, T6, T7, T8]
-    times = [10, 5, 3, 5, 20, 5, 3, 8]
-    gripper_states = [0, 0, 1, 1, 1, 1, 0, 0]
+    goals = [T1, T2]
+    times = [10,5]
+    gripper_states = [0,0]
+    # goals = [T1, T2, T3, T4, T5, T6, T7, T8]
+    # times = [10, 5, 3, 5, 20, 5, 3, 8]
+    # gripper_states = [0, 0, 1, 1, 1, 1, 0, 0]
 
     return goals, times, gripper_states
 
@@ -70,29 +80,18 @@ def array_to_transformation_matrix(arr):
 
 def main():
     q_base_init = [0, 0, 0]
-    q_arm_init = [0, 0, 0.2, -1.6, 0]
+    q_arm_init = [1.16, 0, 0.2, -1.6, 0.1]
     wheel_angles = [0] * 4
     robot_kin = Kinematics(q_base_init, q_arm_init, wheel_angles)
     controller = Control(robot_kin)
     print(robot_kin.arm_kin.get_curr_endeffector_transform())
 
-    controller = Control(robot_kin)
-
     start = np.array([
-        [ 0, 0, 1, 0.22],
-        [ 0, 1, 0, 0.1],
+        [ 0, 0, 1, 0.],
+        [ 0, 1, 0, 0.],
         [-1, 0, 0, 0.5],
         [ 0, 0, 0, 1.0]
     ])
-    th = np.radians(45)
-    rot = np.array([
-        [cos(th), 0, -sin(th), 0],
-        [0      , 1, 0       , 0],
-        [sin(th), 0, cos(th) , 0],
-        [0      , 0, 0       , 1]
-    ])
-
-    start = np.matmul(start, rot)
     goals, times, gripper_states = gen_pickup_dropoff_block_trajectory()
     traj_gen = Trajectory()
     trajectory = traj_gen.generate_trajectories(start, goals, times, gripper_states)
